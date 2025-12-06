5adf78e259e99356771ac87e30bf442e884858be
Millosaurs <millosaurs@gmail.com>
2025-12-06 06:51:27 +0000

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@maybe-will-work/db";
+import * as schema from "@maybe-will-work/db/schema/auth";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "sqlite",
+
+		schema: schema,
+	}),
+	trustedOrigins: [process.env.CORS_ORIGIN || ""],
+	emailAndPassword: {
+		enabled: true,
+	},
+	advanced: {
+		defaultCookieAttributes: {
+			sameSite: "none",
+			secure: true,
+			httpOnly: true,
+		},
+	},
+});
